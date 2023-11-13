6d7f520080c82275da8fb6917a0851a8e8f7e3ee
Tim Collins <38574258+zen-forest@users.noreply.github.com>
2023-11-12 23:48:28 -0500

    created a class based view over functions

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,7 +5,7 @@ app_name = "blog"
 
 urlpatterns = [
   #post view
-  path('', views.post_list, name='post_list'),
+  path('', views.PostListView.as_view(), name='post_list'),
   path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
        views.post_detail, 
        name='post_detail'),
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post 
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
+from django.views.generic import ListView
+
 
 def post_list(request):
     post_list = Post.published.all()
@@ -31,4 +33,9 @@ def post_detail(request, year, month, day, post):
                 {'post': post})
 
 
-
+class PostListView(ListView):
+   # custom QuerySet instead of retrieving all objects
+   queryset = Post.published.all()
+   context_object_name = 'posts'
+   paginate_by = 3
+   template_name = 'blog/post/list.html'

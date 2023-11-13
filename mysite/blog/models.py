0001abf69b487fe8e7dadedfa05a14359839e24e
Tim Collins <38574258+zen-forest@users.noreply.github.com>
2023-11-12 20:06:05 -0500

    improved URLs and added pagination

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse 
 
 class PublishedManager(models.Manager):
 	def get_queryset(self):
@@ -14,7 +14,8 @@ class Post(models.Model):
     PUBLISHED = 'PB', 'Published'
 
   title = models.CharField(max_length=250)
-  slug = models.SlugField(max_length=250)
+  slug = models.SlugField(max_length=250, 
+                          unique_for_date='publish')
 
   # One to many relationship
   # Django will create a foreign key in the database using the primary key of the related model 
@@ -44,3 +45,10 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title 
+  
+  def get_absolute_url(self):
+      return reverse('blog:post_detail', 
+                     args=[self.publish.year,
+                           self.publish.month,
+                           self.publish.day,
+                           self.slug])

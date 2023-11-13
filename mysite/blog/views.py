a3dd55fa84127fe8ee819cd153628c7da32f1069
Tim Collins <38574258+zen-forest@users.noreply.github.com>
2023-11-13 00:25:55 -0500

    end of workday commit. Added forms.py, need to hook up the URL pattern, view, and template, great job Tim

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 from django.core.paginator import Paginator, EmptyPage,\
                                   PageNotAnInteger
 from django.views.generic import ListView
-
+from .forms import EmailPostForm
 
 def post_list(request):
     post_list = Post.published.all()
@@ -39,3 +39,17 @@ class PostListView(ListView):
    context_object_name = 'posts'
    paginate_by = 3
    template_name = 'blog/post/list.html'
+
+def post_share(request, post_id):
+   # retrieve post by id 
+   post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+   if request.method == "POST":
+      # form was submitted 
+      form = EmailPostForm(request.POST)
+      if form.is_valid():
+         #Form fields validation
+         cd = form.cleaned_data
+         #send email
+      else:
+         form=EmailPostForm()
+      return render(request, 'blog/post/share.html', {'post', post, 'form', form})

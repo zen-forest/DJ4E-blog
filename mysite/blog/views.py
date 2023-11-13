43f2ecb61cf4d8817aa9223ab3a5796291c54ec3
Tim Collins <38574258+zen-forest@users.noreply.github.com>
2023-11-12 22:59:32 -0500

    pagination edge case prevention

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post 
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage,\
+                                  PageNotAnInteger
 
 def post_list(request):
     post_list = Post.published.all()
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
+    try:
+      posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(1)
 
     return render(request,
                  'blog/post/list.html',

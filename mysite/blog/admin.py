00de5bf012c5c6fe6c25608638b4df78666c030b
Tim Collins <38574258+zen-forest@users.noreply.github.com>
2023-12-03 12:32:24 -0500

    created model for comments and migrated

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Post
-
+from .models import Comment
 # admin.site.register(Post)
 
 @admin.register(Post)
@@ -13,4 +13,8 @@ class PostAdmin(admin.ModelAdmin):
   date_hierarchy = 'publish'
   ordering = ['status', 'publish']
 
-  
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+  list_display = ['name', 'email','post','created','active']
+  list_filter = ['active', 'created', 'updated']
+  search_fields = ['name', 'email', 'body']

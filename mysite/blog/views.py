19564b43c735fbdb77220bc1006684e6bb6e3f68
Tim Collins <timcollinsdesign+metasignal@gmail.com>
2024-03-18 14:27:56 -0400

    Tagging is not fully implemented with the ability to click on any tag to see results with that specific one.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,9 +6,14 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
+from taggit.models import Tag
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+       tag = get_object_or_404(Tag, slug=tag_slug)
+       post_list = post_list.filter(tags__in=[tag])
     # Pagination with 3 posts per page
     paginator = Paginator(post_list, 4)
     page_number = request.GET.get('page', 1)
